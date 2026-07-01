26db6cadc8ea9ef670c2c5be286eb235762825c3
ErwinCerpa <erwin.cerpa@ascapacitacion.edu.mx>
2026-07-01 15:09:12 -0600

    app.py

--- app.py
+++ app.py
@@ -7,12 +7,41 @@ st.set_page_config(page_title="Simulador Mundial 2026", layout="wide")
 st.title("⚽ Simulador Mundial 2026: Análisis Estadístico")
 
 data = {
-    'Nombre': ['Mexico', 'Corea del Sur', 'Rep. Checa', 'Sudafrica', 'Argentina', 'Marruecos', 'Escocia', 'Bolivia', 'EEUU', 'Suiza', 'Nigeria', 'Panama', 'Brasil', 'Japon', 'Serbia', 'Australia', 'Francia', 'Colombia', 'Grecia', 'Canada', 'Inglaterra', 'Ecuador', 'Turquia', 'Argelia', 'Alemania', 'Chile', 'Islandia', 'Iran', 'España', 'Noruega', 'Ghana', 'Corea del Norte', 'Portugal', 'Dinamarca', 'Tunez', 'Honduras', 'Uruguay', 'Polonia', 'Costa Rica', 'Jamaica', 'Italia', 'Senegal', 'Paraguay', 'Austria', 'Croacia', 'Suecia', 'Egipto', 'Arabia Saudita'],
-    'ELO': [8.2, 7.4, 7.1, 6.5, 9.5, 7.6, 6.8, 6.0, 8.0, 7.5, 7.0, 6.5, 9.3, 7.8, 7.2, 6.9, 9.6, 7.9, 6.9, 7.4, 9.2, 7.5, 7.1, 6.8, 9.0, 7.3, 6.7, 7.0, 9.1, 7.6, 6.9, 6.2, 8.9, 7.7, 6.8, 6.4, 8.5, 7.2, 6.8, 6.6, 8.7, 7.5, 7.0, 7.4, 8.3, 7.5, 7.1, 6.5],
-    'Tactico': [7.6, 7.8, 7.3, 6.0, 9.2, 7.5, 6.9, 5.8, 7.7, 7.6, 7.0, 6.4, 9.0, 8.2, 7.1, 6.8, 9.4, 8.0, 7.0, 7.2, 9.1, 7.4, 7.2, 6.7, 8.9, 7.3, 6.5, 6.9, 9.3, 7.5, 6.8, 6.5, 8.8, 7.6, 6.7, 6.3, 8.3, 7.1, 6.6, 6.5, 8.6, 7.4, 6.9, 7.3, 8.2, 7.4, 7.0, 6.4],
-    'Logistica': [9.5, 6.8, 7.2, 6.0, 8.0, 7.0, 7.5, 6.0, 9.0, 7.3, 6.5, 7.8, 7.5, 6.9, 7.2, 6.5, 7.8, 8.5, 7.0, 8.5, 7.5, 8.2, 7.1, 6.8, 7.6, 8.0, 6.8, 6.5, 7.4, 7.2, 6.5, 6.0, 7.2, 7.3, 6.6, 7.5, 8.0, 7.2, 8.0, 7.5, 7.4, 7.0, 7.8, 7.4, 7.2, 7.2, 6.8, 6.2],
-    'Psicologia': [8.1, 7.7, 7.2, 6.5, 9.6, 8.5, 7.0, 6.0, 7.9, 7.6, 7.8, 6.8, 9.2, 7.8, 7.0, 7.2, 9.3, 8.2, 7.0, 7.5, 8.8, 7.7, 7.3, 7.2, 8.5, 7.8, 6.8, 7.3, 8.6, 7.1, 7.5, 7.5, 8.4, 7.6, 7.0, 7.0, 9.0, 7.1, 7.5, 6.8, 8.2, 8.0, 7.5, 7.0, 9.1, 7.4, 7.6, 6.8]
+    'Nombre': [
+        'Mexico', 'Corea del Sur', 'Rep. Checa', 'Sudafrica', 'Argentina', 'Marruecos', 
+        'Escocia', 'Bolivia', 'EEUU', 'Suiza', 'Nigeria', 'Panama', 'Brasil', 'Japon', 
+        'Serbia', 'Australia', 'Francia', 'Colombia', 'Grecia', 'Canada', 'Inglaterra', 
+        'Ecuador', 'Turquia', 'Argelia', 'Alemania', 'Chile', 'Islandia', 'Iran', 'España', 
+        'Noruega', 'Ghana', 'Corea del Norte', 'Portugal', 'Dinamarca', 'Tunez', 'Honduras', 
+        'Uruguay', 'Polonia', 'Costa Rica', 'Jamaica', 'Italia', 'Senegal', 'Paraguay', 
+        'Austria', 'Croacia', 'Suecia', 'Egipto', 'Arabia Saudita'
+    ],
+    'ELO': [
+        7.9, 7.3, 7.1, 6.5, 9.7, 7.8, 6.7, 6.0, 7.8, 7.6, 7.0, 6.5, 9.1, 7.8, 7.2, 
+        8.0, 9.6, 9.1, 6.9, 7.5, 9.2, 7.6, 8.6, 6.8, 8.8, 7.2, 6.7, 7.0, 9.7, 8.7, 
+        6.9, 6.2, 9.0, 7.7, 6.8, 6.4, 8.6, 7.2, 6.8, 6.6, 8.5, 8.4, 8.3, 7.5, 8.2, 
+        7.5, 7.1, 6.5
+    ],
+    'Tactico': [
+        7.4, 7.8, 7.3, 6.0, 9.4, 7.7, 6.9, 5.8, 7.5, 7.7, 7.0, 6.4, 8.8, 8.3, 7.1, 
+        7.2, 9.4, 8.4, 7.0, 7.4, 9.1, 7.4, 8.0, 6.7, 8.7, 7.1, 6.5, 6.9, 9.6, 7.9, 
+        6.8, 6.5, 8.9, 7.6, 6.7, 6.3, 8.4, 7.1, 6.6, 6.5, 8.4, 7.6, 7.4, 7.5, 8.1, 
+        7.4, 7.0, 6.4
+    ],
+    'Logistica': [
+        9.5, 6.8, 7.2, 6.0, 8.2, 7.1, 7.5, 6.0, 9.0, 7.3, 6.5, 7.8, 7.4, 6.9, 7.2, 
+        6.6, 7.9, 8.6, 7.0, 8.6, 7.6, 8.2, 7.4, 6.8, 7.7, 8.0, 6.8, 6.5, 7.8, 7.4, 
+        6.5, 6.0, 7.3, 7.3, 6.6, 7.5, 8.1, 7.2, 8.0, 7.5, 7.5, 7.2, 7.8, 7.4, 7.2, 
+        7.2, 6.8, 6.2
+    ],
+    'Psicologia': [
+        7.8, 7.7, 7.2, 6.5, 9.7, 8.6, 7.0, 6.0, 7.5, 7.7, 7.8, 6.8, 8.9, 8.0, 7.0, 
+        7.6, 9.2, 8.5, 7.0, 7.8, 8.9, 7.8, 7.9, 7.2, 8.4, 7.6, 6.8, 7.3, 9.0, 7.4, 
+        7.5, 7.5, 8.5, 7.6, 7.0, 7.0, 9.1, 7.1, 7.5, 6.8, 8.1, 8.2, 7.8, 7.1, 9.0, 
+        7.4, 7.6, 6.8
+    ]
 }
+
 df = pd.DataFrame(data)
 
 grupos_data = {
